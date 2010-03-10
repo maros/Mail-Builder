3ca073ad86676a8b647b2acd8dbfb397ab2256ee
Maros Kollar <maros@k-1.com>
2010-03-10 08:30:50 +0100

    Bumped to 1.15; Fixed broken Dist

--- lib/Mail/Builder.pm
+++ lib/Mail/Builder.pm
@@ -6,7 +6,7 @@ use warnings;
 
 use version;
 our $AUTHORITY = 'cpan:MAROS';
-our $VERSION = version->new("1.14");
+our $VERSION = version->new("1.15");
 
 use parent qw(Class::Accessor);
 use Carp;
